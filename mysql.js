b4b34c472bd790e5d19913904cc83cf4586e74ae
SojungLeeee <164296796+SojungLeeee@users.noreply.github.com>
2024-08-07 01:27:17 +0900

    Update mysql.js

--- mysql.js
+++ mysql.js
@@ -3,11 +3,16 @@
 const mysql = require("mysql2/promise"); // Promise 기반 API를 사용
 
 const dbInfo = {
-    host: "localhost",  // 데이터베이스 주소
-    port: 3306,         // 데이터베이스 포트
-    user: "root",       // 로그인 계정
-    password: "1234",   // 비밀번호
-    database: "PKNU_DATA", // 엑세스할 데이터베이스
+    // host: "localhost",  // 데이터베이스 주소
+    // port: 3306,         // 데이터베이스 포트
+    // user: "root",       // 로그인 계정
+    // password: "1234",   // 비밀번호
+    // database: "PKNU_DATA", // 엑세스할 데이터베이스
+    host: process.env.DB_HOST || "localhost",  // 데이터베이스 주소
+    port: process.env.DB_PORT || 3306,         // 데이터베이스 포트
+    user: process.env.DB_USER || "root",       // 로그인 계정
+    password: process.env.DB_PASSWORD || "1234",   // 비밀번호
+    database: process.env.DB_NAME || "PKNU_DATA", // 엑세스할 데이터베이스
 };
 
 let pool; // 데이터베이스 연결 풀
